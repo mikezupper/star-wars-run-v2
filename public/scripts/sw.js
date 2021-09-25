f03dfe03e38a8f37d9cf8d3b7d3d58923f6308e4
Mike Zupper <me@mikezupper.com>
2021-09-25 11:08:27 -0400

    Initial Commit

---
+++ public/scripts/sw.js
@@ -0,0 +1,32 @@
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js"
+);
+
+const { registerRoute } = workbox.routing;
+const { CacheFirst } = workbox.strategies;
+const { CacheableResponse, CacheableResponsePlugin } =
+  workbox.cacheableResponse;
+
+const matchFunction = ({ url, request, event }) => {
+  const match = url.href.includes("https://api.starwars.run/api/");
+  return match;
+};
+
+registerRoute(
+  matchFunction,
+  new CacheFirst({
+    cacheName: "search-results-cache-v1.0.0",
+    plugins: [new CacheableResponsePlugin({ statuses: [0, 200] })],
+  })
+);
+registerRoute(
+  ({ url }) => url.pathname.endsWith(".js") || url.pathname.endsWith(".css"),
+  new CacheFirst({
+    cacheName: "static-cache-1.0.0",
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
