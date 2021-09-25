f0b0e22e9bdb1d3ac9305df41b87a3d3e12ef1ff
Mike Zupper <me@mikezupper.com>
2021-09-25 11:14:27 -0400

    added scope to service worker

--- public/scripts/sw-startup.js
+++ public/scripts/sw-startup.js
@@ -1,5 +1,5 @@
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function () {
-    navigator.serviceWorker.register("scripts/sw.js");
+    navigator.serviceWorker.register("scripts/sw.js", { scope: "/" });
   });
 }
