0a38d40d973c6d20b6644cf561ccc681c71b8948
Mike Zupper <me@mikezupper.com>
2021-10-07 23:03:10 -0400

    added style with materialcss

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -2,8 +2,8 @@ import { fromEvent } from "/scripts/fromEvent.js";
 window.xs = xstream.default;
 
 const { makeDOMDriver } = CycleDOM;
-const { h1, h2, h4, label, hr } = CycleDOM;
-const { div, ul, li } = CycleDOM;
+const { h1, h2, h3, h4, label, hr } = CycleDOM;
+const { div, nav, ul, li, i } = CycleDOM;
 const { a, span, input, button } = CycleDOM;
 const { makeHTTPDriver } = CycleHTTPDriver;
 
@@ -87,26 +87,83 @@ const model = (actions) => {
 const view = (state$) => {
   return state$.map((response) => {
     const { next, previous, results, count } = response;
-    return div([
-      ul([
-        li(".entity-type", "people"),
-        li(".entity-type", "planets"),
-        li(".entity-type", "starships"),
-        li(".entity-type", "species"),
-        li(".entity-type", "vehicles"),
-        li(".entity-type", "films"),
+    return div(".container ", [
+      nav(".nav-wrapper .blue .darken-2 ", [
+        a(
+          ".sidenav-trigger",
+          {
+            attrs: { "data-target": "sw-nav" },
+          },
+          [i(".material-icons", "menu")]
+        ),
+        ul(".left .hide-on-med-and-down", [
+          li(".entity-type ", [a("people")]),
+          li(".entity-type ", [a("starships")]),
+          li(".entity-type ", [a("species")]),
+          li(".entity-type", [a("vehicles")]),
+          li(".entity-type", [a("films")]),
+          li(".entity-type", [a("planets")]),
+        ]),
+        ul(
+          ".sidenav",
+          {
+            attrs: { id: "sw-nav" },
+          },
+          [
+            li(".entity-type  ", [a("people")]),
+            li(".entity-type ", [a("starships")]),
+            li(".entity-type  ", [a("species")]),
+            li(".entity-type ", [a("vehicles")]),
+            li(".entity-type  ", [a("films")]),
+            li(".entity-type", [a("planets")]),
+          ]
+        ),
       ]),
-      hr(),
-      input(".search-term", { attrs: { type: "text" } }),
-      hr(),
-      div([span(".search-count", count)]),
-      a(".search-next", { href: next }, next),
-      a(".search-previous", { href: previous }, previous),
-      ul(results?.map((e) => li(".search-result-entry", e.name || e.title))),
+      div(".row .center", [
+        div(".input-field .m6", [
+          i(".material-icons prefix", "search_term"),
+          label(".label-icon", { atrrs: { for: "search" } }),
+          input(".search-term .validate", {
+            attrs: {
+              type: "text",
+              id: "search",
+            },
+          }),
+        ]),
+      ]),
+      div(".row .center", [
+        ul(".col .s12 ", [
+          previous &&
+            li([
+              a(".search-previous", { attrs: { "data-previous": previous } }, [
+                i(".material-icons", "chevron_left"),
+              ]),
+            ]),
+          count && li([span(".center .search-count", `Count: ${count}`)]),
+          next &&
+            li([
+              a(".search-next", { attrs: { "data-next": next } }, [
+                i(".material-icons", "chevron_right"),
+              ]),
+            ]),
+        ]),
+        div(
+          ".row .center",
+          results?.map((e) =>
+            div(".card .grey .lighten-1 .col .s12 .m6", [
+              div(
+                span(
+                  ".search-result-entry .card-content .white-text",
+                  e.name || e.title
+                )
+              ),
+            ])
+          )
+        ),
+      ]),
     ]);
   });
 };
-
 const drivers = {
   DOM: makeDOMDriver("#main"),
   HTTP: makeHTTPDriver(),
